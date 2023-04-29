a412369e8a8d32a0db6a760375c73b6e35e3e774
yongun2 <djkye0001@naver.com>
2023-04-29 22:41:52 +0900

    [UPDATE] 라우팅 재설정

--- session_auth/urls.py
+++ session_auth/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
-    # path("profile/", include("user_profile.urls")),
+    path("profile/", include("user_profile.urls")),
 ]
