a4e4c319952563ed5615080ec59450d60a6faf85
yongun2 <djkye0001@naver.com>
2023-04-29 22:41:27 +0900

    [FEAT] 프로필 조회 및 수정

---
+++ user_profile/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from user_profile.views import UserProfileView
+
+urlpatterns = [
+    path('', UserProfileView.as_view())
+]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from operator import itemgetter
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from user_profile.models import UserProfile
+from user_profile.serializers import UserProfileSerializer
+
+
+class UserProfileView(APIView):
+    def get(self, request):
+        user = self.request.user
+        username = user.username
+
+        user = User.objects.get(id=user.id)
+
+        user_profile = UserProfile.objects.get(user_id=user.id)
+        user_profile = UserProfileSerializer(user_profile)
+
+        return Response(data={'profile': user_profile.data, 'username': str(username)}, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        user = self.request.user
+        username = user.username
+
+        data = self.request.data
+        name, phone, nickname = itemgetter('name', 'phone', 'nickname')(data)
+
+        user = User.objects.get(id=user.id)
+
+        UserProfile.objects.filter(user_id=user.id).update(name=name, phone=phone, nickname=nickname)
+
+        user_profile = UserProfile.objects.get(user_id=user.id)
+        user_profile = UserProfileSerializer(user_profile)
+
+        return Response(data={'profile': user_profile.data, 'username': str(username)}, status=status.HTTP_200_OK)
