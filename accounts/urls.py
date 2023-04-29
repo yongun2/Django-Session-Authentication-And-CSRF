810b2ecb4bb526de551152fed4946911676e2747
yongun2 <djkye0001@naver.com>
2023-04-29 18:21:04 +0900

    [FEAT] 로그인, 로그아웃 기능

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accounts.views import SignupView, GetCSRFToken, CheckDuplicateView
+from accounts.views import SignupView, GetCSRFToken, CheckDuplicateView, CheckAuthenticatedView, LoginView, LogoutView
 
 urlpatterns = [
+    path('authenticated', CheckAuthenticatedView.as_view()),
     path('duplicate', CheckDuplicateView.as_view()),
     path('register', SignupView.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
     path('csrf_cookie', GetCSRFToken.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
 
+from django.contrib import auth
 from django.contrib.auth.models import User
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
@@ -8,6 +9,20 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from user_profile.models import UserProfile
+
+
+class CheckAuthenticatedView(APIView):
+    def get(self, request):
+        try:
+            isAuthenticated = User.is_authenticated
+
+            if isAuthenticated:
+                return Response(data={"isAuthenticated": "true"}, status=status.HTTP_200_OK)
+            else:
+                return Response(data={"isAuthenticated": "false"}, status=status.HTTP_403_FORBIDDEN)
+        except:
+            return Response(data={"error": "Something went wrong when checking authentication status"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @method_decorator(csrf_protect, name='dispatch')
@@ -23,20 +38,24 @@ class SignupView(APIView):
         if User.objects.filter(username=username).exists():
             return Response({'error': "username already exists"}, status=status.HTTP_409_CONFLICT)
 
-        user = User.objects.create_user(username=username, password=password)
-        user.save()
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
 
-        user = User.objects.get(username=username)
-        user_profile = UserProfile(user=user, name=name, phone=phone, nickname=nickname)
-        user_profile.save()
+            user = User.objects.get(username=username)
+            user_profile = UserProfile(user=user, name=name, phone=phone, nickname=nickname)
+            user_profile.save()
 
-        return Response(data={"username": username, "name": name, "phone": phone, "nickname": nickname},
-                        status=status.HTTP_201_CREATED)
+            return Response(data={"username": username, "name": name, "phone": phone, "nickname": nickname},
+                            status=status.HTTP_201_CREATED)
+        except:
+            return Response(data={"error": "Something went wrong while registering "},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @method_decorator(csrf_protect, name='dispatch')
 class CheckDuplicateView(APIView):
-    permission_classes = (permissions.AllowAny, )
+    permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
         data = request.data
@@ -46,6 +65,39 @@ class CheckDuplicateView(APIView):
             return Response(data={"username": username}, status=status.HTTP_409_CONFLICT)
         else:
             return Response(data={"username": username}, status=status.HTTP_200_OK)
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        data = self.request.data
+
+        username, password = itemgetter('username', 'password')(data)
+
+        try:
+
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return Response(data={"username": username}, status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response(data={"error": "Something went wrong when login"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            auth.logout(request)
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response({'error': 'something went wrong when logging out'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')
